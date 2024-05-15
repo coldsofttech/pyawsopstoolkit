2c7958149d6aa7f818bf760a4cafefddb5cad633
coldsofttech <coldsoft.mail@gmail.com>
2024-05-15 22:03:34 +0100

    Added pyawsopstoolkit.exceptions package

---
+++ readme/EXCEPTIONS.md
@@ -0,0 +1,26 @@
+# pyawsopstoolkit.exceptions
+
+This package offers a collection of exception classes tailored for handling errors within the AWS Ops Toolkit. These
+exceptions are specifically designed to address different scenarios and errors that may occur during the execution of
+pyawsopstoolkit operations, providing comprehensive support for error handling and debugging within the toolkit.
+
+# Documentation
+
+## AssumeRoleError
+
+Custom exception class designed for AWS Ops Toolkit. This exception is typically raised when there's a failure during
+the assumption of a role session.
+
+### Methods
+
+- `__init__(self, role_arn: str, exception: Optional[Exception] = None) -> None`: Constructor for the AssumeRoleError
+  class.
+
+## ValidationError
+
+Custom exception class for AWS Ops Toolkit. This exception is typically raised when validation fails.
+
+### Methods
+
+- `__init__(self, message: str, exception: Optional[Exception] = None) -> None`: Constructor for the ValidationError
+  class.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+import pyawsopstoolkit
+
+setup(
+    name=pyawsopstoolkit.__name__,
+    version=pyawsopstoolkit.__version__,
+    packages=[
+        pyawsopstoolkit.__name__,
+        pyawsopstoolkit.exceptions.__name__
+    ],
+    url='https://github.com/coldsofttech/pyawsopstoolkit.git',
+    license='MIT',
+    author='coldsofttech',
+    description=pyawsopstoolkit.__description__,
+    requires_python=">=3.10",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=[
+        "aws", "toolkit", "operations", "tools", "development", "python", "validation", "session-management",
+        "utilities", "enhancements", "integration", "amazon-web-services"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
+    ]
+)
