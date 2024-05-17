7e387380ad480ad0d303999d13a6963995f491b9
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-05-17 19:31:31 +0100

    Added Account, Credentials, and Session (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Version History
 
-- 0.1.1: Added [pyawsopstoolkit.validators](pyawsopstoolkit/validators) package. (latest)
+- 0.1.2: Added Account, Credentials, and Session. (latest)
+- 0.1.1: Added [pyawsopstoolkit.validators](pyawsopstoolkit/validators) package.
 - 0.1.0: Added [pyawsopstoolkit.exceptions](pyawsopstoolkit/exceptions) package.
--- pyawsopstoolkit/__init__.py
+++ pyawsopstoolkit/__init__.py
@@ -1,9 +1,12 @@
 __all__ = [
+    "Account",
+    "Credentials",
+    "Session",
     "exceptions",
     "validators"
 ]
 __name__ = "pyawsopstoolkit"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __description__ = """
 This extensive package, AWS Ops Toolkit, offers a wide range of features and enhancements designed to streamline
 and optimize interactions with Amazon Web Services (AWS). As of now, the toolkit includes a robust set of
@@ -14,3 +17,5 @@ ecosystem, complete with a versatile assume role function.
 As we continue to evolve, watch this space for upcoming additions and enhancements, promising even greater utility
 and efficiency in AWS-related workflows.
 """
+
+from pyawsopstoolkit.__main__ import Credentials, Account, Session
